9fae0164ed2e72eedf48e5e77b881293e77c4266
AndresEstupinan <af.estupinan@uniandes.edu.co>
2018-10-28 16:39:55 -0500

    La grafica mejoro, pero tal vez se pueda mejorar mas. Hice las interpolaciones de la funcion

--- Fourier.py
+++ Fourier.py
@@ -1,13 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft, fftfreq
-
+from scipy.interpolate import interp1d
+import scipy as sc
+from scipy import interpolate
 
 #1
 
 
 A=np.genfromtxt("signal.dat", delimiter=",")
-B=np.genfromtxt("incompletos.dat")
+B=np.genfromtxt("incompletos.dat", delimiter=",")
 
 
 #2
@@ -27,7 +29,7 @@ plt.savefig("EstupinanAndres_signal.pdf")
 
 n=len(Ax)
 m=list()
-dt=Ax[1]-Ax[2]
+dt=Ax[2]-Ax[1]
 s=0.0
 for b in range(n):
 
@@ -35,7 +37,7 @@ for b in range(n):
 
 	s=0.0
 	for k in range(n):
-		s=s+(Ay[k]*np.exp((-1j)*2.0*np.pi*k*(b/n)))
+		s=s+(Ay[k]*np.exp((-1j)*2.0*np.pi*k*(float(b)/float(n))))
 	
 #4
 
@@ -43,9 +45,10 @@ for b in range(n):
 
 
 #fft_x= fft(Ay)
+#plt.figure()
 freq= fftfreq(n,dt)
-#plt.plot(freq,abs(fft_x))
-#plt.show()
+#plt.plot(freq,(fft_x))
+#plt.savefig("transfourierO.pdf")
 #Solo para comparar
 plt.figure()
 plt.plot(freq,np.abs(m), label="transformada de fourier de los datos")
@@ -63,24 +66,86 @@ print ("las frecuencias principales de la señal son los picos mas altos de la g
 for x in range(len(freq)):
 	if(abs(freq[x])>1000):
 		m[x]=0
+#for x in range(len(freq)):
+#	if(abs(freq[x])>1000):
+#		fft_x[x]=0
 
-
-plt.figure()
-plt.plot(freq,np.abs(m))
-plt.show()
-#para ver la tranasformada filtrada
+#plt.figure()
+#plt.plot(freq,np.abs(m))
+#plt.show()
+#para ver la transformada filtrada
 
 filtrada= np.fft.ifft(m)
+#filtrada2= np.fft.ifft(fft_x)
+
+#plt.figure()
+#plt.plot(Ax, np.real(filtrada2)+np.imag(filtrada2), label="Signal filtrada2")
+#plt.plot(Ax, Ay)
+#plt.savefig("EstupinanAndres_filtrada2.pdf")
+
 
 plt.figure()
-plt.plot(Ax, filtrada)
+plt.plot(Ax, np.real(filtrada)+np.imag(filtrada), label="Signal filtrada")
 plt.plot(Ax, Ay)
-plt.show()
+plt.savefig("EstupinanAndres_filtrada.pdf")
 
+#7
 
+print (" No se puede hacer la transformada de fourier de datos incompletos, pues la tr")
 
 
 	
+#8
+
+B1=B[:,0]
+B2=B[:,1]
+
+f1=interp1d(B1,B2, kind="quadratic")
+f2=interp1d(B1,B2, kind="cubic")
+
+plt.figure()
+plt.plot(B1,B2)
+#plt.plot(B1,f1)
+#plt.plot(B1,f2)
+plt.show()
+
+#print (f1)
+
+X=np.linspace(0,0.028515625,1000)
+def cuadraticsplines(A,B,X):
+	cuadraticBspline = sc.interpolate.splrep(A, B, k=2)
+	return sc.interpolate.splev(X ,cuadraticBspline)
+
+
+def cubicsplines(A,B,X):
+	cubicBspline = sc.interpolate.splrep(A, B, k=3)
+	return sc.interpolate.splev(X ,cubicBspline)
+
+plt.figure()
+plt.scatter(B1,B2)
+plt.plot(X, cuadraticsplines(B1,B2,X))
+plt.plot(X, cubicsplines(B1,B2,X))
+plt.legend(['Cuadratic inter','Cubic inter', 'Puntos'])
+plt.savefig("EstupinanAndres_Interpola.pdf")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
