ab9e392a9ba963efbabe5b299a27d62955719894
AndresEstupinan <af.estupinan@uniandes.edu.co>
2018-10-27 20:13:23 -0500

    Me canse de fracasar con PCA, entonces empece Fourier. Obtencion de datos y graficas

--- Fourier.py
+++ Fourier.py
@@ -1,3 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+#1
+
+
+A=np.genfromtxt("signal.dat", delimiter=",")
+B=np.genfromtxt("incompletos.dat")
+
+
+#2
+
+Ax=A[:,0]
+Ay=A[:,1]
+
+plt.figure()
+plt.plot(Ax,Ay, label= "datos de signal.dat")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.legend()
+plt.savefig("EstupinanAndres_signal.pdf")	
+
+
+#3
+
