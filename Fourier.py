3664d026a50d8f2f6a00b15bd6ad3730fcbe9733
AndresEstupinan <af.estupinan@uniandes.edu.co>
2018-10-28 15:27:58 -0500

    Hice la señal filtrada, pero no se ajusta a algunos puntos en la grafica

--- Fourier.py
+++ Fourier.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.fftpack import fft, fftfreq
 
 
 #1
@@ -23,4 +24,72 @@ plt.savefig("EstupinanAndres_signal.pdf")
 
 
 #3
+
+n=len(Ax)
+m=list()
+dt=Ax[1]-Ax[2]
+s=0.0
+for b in range(n):
+
+	m.append(s)
+
+	s=0.0
+	for k in range(n):
+		s=s+(Ay[k]*np.exp((-1j)*2.0*np.pi*k*(b/n)))
+	
+#4
+
+
+
+
+#fft_x= fft(Ay)
+freq= fftfreq(n,dt)
+#plt.plot(freq,abs(fft_x))
+#plt.show()
+#Solo para comparar
+plt.figure()
+plt.plot(freq,np.abs(m), label="transformada de fourier de los datos")
+plt.xlabel("Frecuencias")
+plt.ylabel("F(f(t))")
+plt.legend()
+plt.savefig("EstupinanAndres_TF.pdf")
+
+#5
+
+print ("las frecuencias principales de la señal son los picos mas altos de la grafica. En ese caso son los picos que superan el valor de 500 en el eje y, y estan cerca al 0 de frecuencias")
+
+#6
+
+for x in range(len(freq)):
+	if(abs(freq[x])>1000):
+		m[x]=0
+
+
+plt.figure()
+plt.plot(freq,np.abs(m))
+plt.show()
+#para ver la tranasformada filtrada
+
+filtrada= np.fft.ifft(m)
+
+plt.figure()
+plt.plot(Ax, filtrada)
+plt.plot(Ax, Ay)
+plt.show()
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
 
