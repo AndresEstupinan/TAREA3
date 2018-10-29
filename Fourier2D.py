cebed9356fd2e350879875a5695799d299653ff7
AndresEstupinan <af.estupinan@uniandes.edu.co>
2018-10-28 21:15:51 -0500

    Leer datos y transformada de fourier 2D

--- Fourier2D.py
+++ Fourier2D.py
@@ -1,3 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import fftpack
+from matplotlib.colors import NoNorm
+from matplotlib.colors import LogNorm
+
+# 1
+
+arbol= plt.imread("arbol.png")
+arbolf= fftpack.fft2(arbol)
+
+#2
+
+plt.figure()
+plt.imshow(np.abs(arbolf),cmap="gray")#, norm=NoNorm()) 
+plt.colorbar()
+plt.savefig("EstupinanAndres_FT2D.pdf")
+
+
+			
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
