06e61dd5d707a90d7c1b69c346d59484b5f4b17b
AndresEstupinan <af.estupinan@uniandes.edu.co>
2018-10-28 21:40:31 -0500

    Posible Filtro exitoso

--- Fourier2D.py
+++ Fourier2D.py
@@ -17,7 +17,19 @@ plt.colorbar()
 plt.savefig("EstupinanAndres_FT2D.pdf")
 
 
-			
+#3
+
+print (arbolf[:,1])
+for x in range(len(arbolf[:,1])):
+	for y in range(len(arbolf[1,:])):
+		if(np.real(arbolf[x,y])>1000):
+			arbolf[x,y]=0
+
+
+plt.figure()
+plt.imshow(np.abs(arbolf),norm=LogNorm())#, norm=NoNorm()) 
+plt.colorbar()
+plt.savefig("EstupinanAndres_FT2D_filtrada.pdf")			
 
 
 
